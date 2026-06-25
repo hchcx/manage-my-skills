b8dae91c9612557f57b8a01755c42144b7c6842c
阿豪 <1719422449@qq.com>
2026-06-25 11:54:17 +0800

    chore(release): 发布 v0.2.5 版本，强制 Windows 以管理员启动并支持 macOS 双平台流水线打包

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,27 @@
 fn main() {
-    tauri_build::build()
+    let mut windows = tauri_build::WindowsAttributes::new();
+    
+    // 定义清单文件，设置要求管理员特权以运行
+    let manifest = r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*" />
+    </dependentAssembly>
+  </dependency>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#;
+
+    windows = windows.app_manifest(manifest);
+
+    tauri_build::try_build(
+        tauri_build::Attributes::new().windows_attributes(windows)
+    ).expect("failed to run build script");
 }
